0aabc58d84c84636670965bba7ae72f41cd56f49
rgmenezes <faelgmp@gmail.com>
2024-05-18 20:21:52 -0300

    finalizando rodape

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import MainHeader from '@/components/layouts/MainHeader';
 import './globals.css';
 import styles from './layout.module.css';
 import { useEffect, useState } from 'react';
+import Footer from '@/components/layouts/Footer';
 
 export default function RootLayout({
   children,
@@ -17,6 +18,7 @@ export default function RootLayout({
       <body suppressHydrationWarning={true} className={`${styles.body} ${styles[theme]}`}>
         <MainHeader theme={theme} setTheme={setTheme}/>
         {children}
+        <Footer />
       </body>
     </html>
   );
---
+++ src/components/layouts/Footer.tsx
@@ -0,0 +1,30 @@
+import TextLink from '../link/TextLink';
+import Logo from '../svg/Logo';
+import styles from './Footer.module.css';
+import Section from './Section';
+
+export default function Footer(){
+  return(
+    <footer>
+      <section className={styles.ad_section}>
+        <p className={`${styles.ad_text} ${styles.ad_text0}`}>TypeScript</p>
+        <p className={`${styles.ad_text} ${styles.ad_text1}`}>MongoDb</p>
+        <p className={`${styles.ad_text} ${styles.ad_text2}`}>JavaScript</p>
+        <p className={`${styles.ad_text} ${styles.ad_text3}`}>Design</p>
+        <p className={`${styles.ad_text} ${styles.ad_text4}`}>Postgres</p>
+      </section>
+
+      <Section className={styles.content_container}>
+        <Logo className={styles.logo}/>
+        <ul className={styles.list_container}>
+          <li><TextLink to='/sobre'>Sobre</TextLink></li>
+          <li><TextLink to='/'>Home</TextLink></li>
+          <li><TextLink to='/contato'>Contato</TextLink></li>
+          <li><TextLink to='/layouts'>Layouts</TextLink></li>
+        </ul>
+        <p>E-mail:<br/><TextLink type='strong' to='/midias'>tart.a.r.contato@gmail.com</TextLink></p>
+        <h5>© TART | 2024</h5>
+      </Section>
+    </footer>
+  );
+};
