fa953fe765dada92a2b2542818bac2fb85a8548f
rgmenezes <faelgmp@gmail.com>
2024-05-20 19:16:19 -0300

    iniciando aba layouts

---
+++ src/app/layouts/page.tsx
@@ -0,0 +1,13 @@
+import Section from '@/components/layouts/Section';
+import styles from './page.module.css';
+
+export default function Layouts(){
+  return(
+    <main className={styles.main}>
+      <h1>Layouts</h1>
+      <Section>
+
+      </Section>
+    </main>
+  );
+};
